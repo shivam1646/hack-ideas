2dcee97376b35efaff6cb860d79d7f0e38005787
shivam1646 <shivamkotak16.sk@gmail.com>
2021-09-29 22:55:20 +0530

    project structure

---
+++ src/layout/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <Header />
+    <v-main>
+      <router-view />
+    </v-main>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Header from './header.vue';
+import Footer from './footer.vue'
+export default {
+  name: 'layout',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+// insert styles here
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import AppLayout from "@/layout";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: (h) => h(AppLayout),
+}).$mount("#app");
