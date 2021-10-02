268266faf58d7bd94d7017d357e4873f4a514ad3
shivam1646 <shivamkotak16.sk@gmail.com>
2021-10-03 00:52:55 +0530

    add components for toolbar and idea-preview in home

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,9 +1,52 @@
 <template>
-  <div>Home</div> 
+  <div>
+    <toolbar />
+    <idea-preview
+      v-for="idea in sortedIdeas"
+      :idea="idea"
+      :key="idea.id"
+    />
+  </div>
 </template>
 
 <script>
+import IdeaPreview from './idea-preview.vue';
+import Toolbar from './toolbar.vue';
+import { mapState, mapActions } from 'vuex';
+import { getVotesCount } from '../../utils';
+
 export default {
-  name: 'home'
+  name: 'home',
+
+  components: { IdeaPreview, Toolbar },
+
+  created() {
+    this.getIdeas();
+  },
+
+  computed: {
+    ...mapState('upvote', ['upvotes']),
+    ...mapState(['ideas', 'sortByField']),
+    sortedIdeas() {
+      if (this.sortByField === 'createdAt') {
+        return [...this.ideas].sort((a, b) => {
+          return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        });
+      }
+      return [...this.ideas].sort((a, b) => {
+        return getVotesCount(b, this.upvotes) - getVotesCount(a, this.upvotes)
+      });
+    }
+  },
+
+  methods: {
+    ...mapActions(['getIdeas'])
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.count {
+  color: $secondary-text-color;
+}
+</style>
