a3c052255887ee09ef28a4471d8e0e52ed792419
shivam1646 <shivamkotak16.sk@gmail.com>
2021-10-02 12:20:05 +0530

    mock data for idea unit test cases

--- mock-data.js
+++ mock-data.js
@@ -1,8 +1,21 @@
 export const INITIAL_STATE = {
   employeeId: '',
-  loggedInUser: null
+  loggedInUser: null,
+  ideas: []
 };
+
+export const INITIAL_IDEA_STATE = () => ({
+  title: '',
+  description: '',
+  tags: []
+});
 
 export const EMPLOYEE_ID = 'HI-JD101';
 
 export const INVALID_EMPLOYEE_ID = '123';
+
+export const IDEA_TITLE = 'JS Hackathon';
+
+export const IDEA_DESCR = 'Apply your JS skills';
+
+export const TAG_IDS = [1, 2];
