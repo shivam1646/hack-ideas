8526f166fcd561d206469fb2bf5c9b4ab88557bd
shivam1646 <shivamkotak16.sk@gmail.com>
2021-10-03 01:35:51 +0530

    data in const file

---
+++ data.js
@@ -0,0 +1,37 @@
+export const TAGS = [
+  {
+    key: 1,
+    value: 'React'
+  }, {
+    key: 2,
+    value: 'Vue'
+  }, {
+    key: 3,
+    value: 'Angular'
+  }, {
+    key: 4,
+    value: 'Javascript'
+  }, {
+    key: 5,
+    value: 'Typescript'
+  }, {
+    key: 6,
+    value: 'AWS'
+  }, {
+    key: 7,
+    value: 'Docker'
+  }
+];
+
+export const TABS = [
+  {
+    id: 0,
+    title: 'Created at',
+    value: 'createdAt',
+  },
+  {
+    id: 1,
+    title: 'Upvotes count',
+    value: 'upvotes'
+  }
+]
